66b48c6986c49f1a9a7c6fc5bce553d5da9c8c93
Caleb Branson <cbransonc@protonmail.com>
2022-09-27 18:26:18 -0400

    Intial commit

---
+++ traveling-salesman/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  console.log("Hello world!");
+  console.log(windowWidth, windowHeight);
+}
+
+function draw() {
+  // put drawing code here
+  background(0);
+}
