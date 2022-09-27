34bbfb7e4c8e692b02c5c0319b079b5c9bc1622c
Caleb Branson <cbransonc@protonmail.com>
2022-09-27 18:50:24 -0400

    Generate and draw

--- traveling-salesman/sketch.js
+++ traveling-salesman/sketch.js
@@ -1,11 +1,40 @@
+let cities = [];
+let order = [];
+
+let bestDist = Infinity;
+let bestOrder = [];
+
+let citiesNum = 5;
+let radius = 10;
+
 function setup() {
-  // put setup code here
   createCanvas(windowWidth, windowHeight);
-  console.log("Hello world!");
-  console.log(windowWidth, windowHeight);
+
+  // Intialize data
+  for (let i = 0; i < citiesNum; i++) {
+    let x = floor(random(width));
+    let y = floor(random(height));
+    cities.push(createVector(x, y));
+    order.push(i);
+  }
 }
 
 function draw() {
-  // put drawing code here
-  background(0);
+  background(200);
+
+  // Draw points
+  noFill();
+  stroke(50);
+  strokeWeight(2);
+  for (let i = 0; i < cities.length; i++) {
+    ellipse(cities[i].x, cities[i].y, radius, radius);
+  }
+
+  // Draw order path
+  beginShape();
+  for (let i = 0; i < order.length; i++) {
+    let city = cities[order[i]];
+    vertex(city.x, city.y);
+  }
+  endShape();
 }
